8be72dda7bc982e78d7ee7152f46e8fa5a137707
chenjiasheng <876833508@qq.com>
2018-02-03 23:54:26 +0800

    修改数据库连接配置

--- src/main/java/com/gdut/safecuit/device/service/CircuitService.java
+++ src/main/java/com/gdut/safecuit/device/service/CircuitService.java
@@ -27,7 +27,9 @@ public class CircuitService extends BaseServiceImpl<Circuit> {
 	private DeviceMapper deviceMapper;
 
 	public int insertCircuit(Circuit circuit){
+		synchronized (circuit){
 
+		}
 		Integer circuit_total = circuitMapper.selectCircuitCountByDeviceId(circuit.getDeviceId());
 		circuit.setCircuitNo(++circuit_total);
 		return circuitMapper.insertSelective(circuit);
